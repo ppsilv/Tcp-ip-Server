f70d6daab78e91f0ff79faf80f4d070c1b22ed46
Paulo da Silva <ppsilv@gmail.com>
2017-05-23 15:49:23 -0300

    Create sc_server.c
    This is a sigle client server in c.

---
+++ sc_server/sc_server.c
@@ -0,0 +1,79 @@
+/*
+    C socket server example
+*/
+ 
+#include<stdio.h>
+#include<string.h>    
+#include<sys/socket.h>
+#include<arpa/inet.h> 
+#include<unistd.h>    
+ 
+#include "msg_probe.h"
+ 
+int main(int argc , char *argv[])
+{
+    int socket_desc , client_sock , c , read_size;
+    struct sockaddr_in server , client;
+    char client_message[20000], buff[512];
+    int size=0; 
+    //Create socket
+    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+    if (socket_desc == -1)
+    {
+        printf("Could not create socket");
+    }
+    puts("Socket created");
+     
+    //Prepare the sockaddr_in structure
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons( 9888 );
+     
+    //Bind
+    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        //print the error message
+        perror("bind failed. Error");
+        return 1;
+    }
+    puts("bind done");
+     
+    //Listen
+    listen(socket_desc , 3);
+     
+    //Accept and incoming connection
+    puts("Waiting for incoming connections...");
+    c = sizeof(struct sockaddr_in);
+reacept:
+    //accept connection from an incoming client
+    client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
+    if (client_sock < 0)
+    {
+        perror("accept failed");
+        return 1;
+    }
+    puts("Connection accepted");
+     
+    //Receive a message from client
+    while( (read_size = recv(client_sock , client_message , 20000 , 0)) > 0 ){
+        //Send the message back to client
+		puts(client_message);
+		size = strlen(client_message);
+		memcpy( buff, (char *)&size + 2, 2);
+		printf("size to sent [%02d] [%02X]",size,size);
+        write(client_sock , buff , 2 );
+        write(client_sock , client_message , strlen(client_message));
+    }
+     
+    if(read_size == 0)
+    {
+        puts("Client disconnected");
+        fflush(stdout);
+    }
+    else if(read_size == -1)
+    {
+        perror("recv failed");
+    }
+     goto reacept;
+    return 0;
+}
